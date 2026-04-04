f73cd4f5c2f3566e8066119656d504527dd38f42
BuddhikaSankalpa <Buddhikasankalpa2003@gmail.com>
2026-04-04 22:23:19 +0530

    without .env file

---
+++ middlewares/uploadVideo.js
@@ -0,0 +1,17 @@
+// import multer from 'multer'
+// import { CloudinaryStorage } from 'multer-storage-cloudinary'
+// import cloudinary from '../config/cloudinary.js'
+
+// // Cloudinary storage config
+// const storage = new CloudinaryStorage({
+//     cloudinary : cloudinary,
+//     params : {
+//         folder : 'course_videos',        // Cloudinary folder name
+//         resource_type : 'video',          // video upload
+//         allowed_formats : ['mp4', 'mkv', 'avi', 'mov'],
+//     }
+// })
+
+// const uploadVideo = multer({ storage : storage })
+
+// export default uploadVideo
