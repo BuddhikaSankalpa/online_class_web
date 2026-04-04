368a97e3f758c284a1f2defbcc0036640d32287a
BuddhikaSankalpa <Buddhikasankalpa2003@gmail.com>
2026-04-04 13:17:06 +0530

    Cloudinary Integration files create ✏️

--- middlewares/checkCourseAccess.js
+++ middlewares/checkCourseAccess.js
@@ -1,35 +1,35 @@
-import Enrollment from "../models/enrollment.js"
+// import Enrollment from "../models/enrollment.js"
 
-export default async function checkCourseAccess(req, res, next){
+// export default async function checkCourseAccess(req, res, next){
 
-    try{
+//     try{
 
-        // user must be logged in to access course content
-        if(req.user == null){
-            res.status(401).json({message : "Login required"})
-            return
-        }
+//         // user must be logged in to access course content
+//         if(req.user == null){
+//             res.status(401).json({message : "Login required"})
+//             return
+//         }
 
-        const userId = req.user.email
-        const courseId = req.params.courseId
+//         const userId = req.user.email
+//         const courseId = req.params.courseId
 
-        // check enrollment
-        const enrollment = await Enrollment.findOne({
-            userId : userId,
-            courseId : courseId
-        })
+//         // check enrollment
+//         const enrollment = await Enrollment.findOne({
+//             userId : userId,
+//             courseId : courseId
+//         })
 
-        // if user is not enrolled, deny access
-        if(enrollment == null){
-            res.status(403).json({message : "You must buy this course first"})
-            return
-        }
+//         // if user is not enrolled, deny access
+//         if(enrollment == null){
+//             res.status(403).json({message : "You must buy this course first"})
+//             return
+//         }
 
-        // if user enrolled, allow access
-        next()
+//         // if user enrolled, allow access
+//         next()
 
-    }catch(err){
-        res.status(500).json({message : err.message})
-    }
+//     }catch(err){
+//         res.status(500).json({message : err.message})
+//     }
 
-}
+// }
